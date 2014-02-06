82b81efd38079d9cdef8876acde02a3b9f2a303b
rasmusstorjohann-AI <rasmus.storjohann@aquaticinformatics.com>
2014-02-06 10:54:27 -0800

    Created a change in a feature branch, now try to build this

--- application/WaterQuality.WebService/src/main/java/com/aquaticinformatics/gaia/App.java
+++ application/WaterQuality.WebService/src/main/java/com/aquaticinformatics/gaia/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Brave New World!" );
+        System.out.println( "Hello Brave Newer World!" );
     }
 }
